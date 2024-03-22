877201a233b72d00408155ca7b5bc82637eeaf56
DillanKling <lfsemcnpod@outlook.com>
2024-03-22 12:28:20 +0800

    molestiae

---
+++ examples/react/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { useWeb3Modal, Web3Button, Web3NetworkSwitch } from '@web3modal/react'
+import { useAccount } from 'wagmi'
+import ThemeControls from '../components/ThemeControls'
+
+export default function HomePage() {
+  const { isConnected } = useAccount()
+  const { open } = useWeb3Modal()
+
+  return (
+    <>
+      <h2>Buttons</h2>
+      <div className="container">
+        {/* Use predefined button */}
+        <Web3Button />
+        <Web3Button balance="show" icon="hide" label="No Icon" />
+        {!isConnected && <Web3Button label="Custom Label" />}
+
+        {/* Alternatively Use custom button */}
+        {!isConnected && <button onClick={() => open()}>Custom Button</button>}
+
+        <Web3NetworkSwitch />
+      </div>
+
+      <ThemeControls />
+    </>
+  )
+}
